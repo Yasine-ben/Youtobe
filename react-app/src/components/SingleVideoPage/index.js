87b6b0d5ebc091f1f97932bf47debb597d308f13
Yasine Omar Benzekri <105954797+Yasine-ben@users.noreply.github.com>
2023-04-20 12:14:56 -0400

    added recomended on right (maybe randomize or make only videos from that creator)

--- react-app/src/components/SingleVideoPage/index.js
+++ react-app/src/components/SingleVideoPage/index.js
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router-dom";
-import { thunkSingleVideo } from '../../store/video';
+import { thunkAllVideos, thunkSingleVideo } from '../../store/video';
 import ReactPlayer from 'react-player';
 
 import './SingleVideoPage.css'
@@ -11,6 +11,7 @@ function SingleVideoPage() {
     const history = useHistory()
     const { video_id } = useParams();
     const video = Object.values(useSelector(state => state.videos.singleVideo))[0]
+    const allVideos = Object.values(useSelector(state => state.videos.allVideos))
     const user = useSelector(state => state.session.user)
 
     const [url, setUrl] = useState('')
@@ -41,26 +42,47 @@ function SingleVideoPage() {
 
     useEffect(() => {
         dispatch(thunkSingleVideo(video_id))
+        dispatch(thunkAllVideos())
     }, [dispatch, user])
 
     return (
         <div className='VP-Wrapper'>
-            <div className='VP-Player-Wrapper'>
-                <ReactPlayer
-                    url={url}
-                    controls={true}
-                    className='VP-Player'
-                    width={windowWidth}
-                    height={720}
-                // onEnded={((e) => '')}
-                />
-            </div>
-            <div className='VP-UnderPlayer-Wrapper'>
-                <div className='VP-Title-Wrapper'>
-                    <p className='VP-Title'>{title}</p>
+            <div className='VP-Left-Wrapper'>
+                <div className='VP-Player-Wrapper'>
+                    <ReactPlayer
+                        url={url}
+                        controls={true}
+                        className='VP-Player'
+                        width={1180}
+                        height={720}
+                    // onEnded={((e) => '')}
+                    />
                 </div>
-                <div className='VP-Lower-Wrapper'>
+                <div className='VP-UnderPlayer-Wrapper'>
+                    <div className='VP-Title-Wrapper'>
+                        <p className='VP-Title'>{title}</p>
+                    </div>
+                    <div className='VP-Lower-Wrapper'>
+                        <div className='VP-Creator-Wrapper'>
 
+                        </div>
+                        <div className='VP-'>
+
+                        </div>
+                    </div>
+                </div>
+                <div className='VP-DescriptionBox-Wrapper'>
+
+                </div>
+            </div>
+            
+            <div className='VP-Right-Wrapper'>
+                <div className='VP-Recomended-Wrapper'>
+                    {allVideos.map((video, idx) => (
+                        <div key={`Recomended_${idx}`} className='VP-Rec-Card-Wrapper'>
+                            <img src={video.thumbnail} alt='recomeneded thumbnail' className='VP-Recomended-Img' />
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
