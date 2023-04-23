0a37f4559d0ea3a3ddfdc96bd59c394f6d787f8b
Yasine Omar Benzekri <105954797+Yasine-ben@users.noreply.github.com>
2023-04-22 22:13:46 -0400

    inputs styled

--- react-app/src/components/Forms/VideoForm/index.js
+++ react-app/src/components/Forms/VideoForm/index.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './VideoForm.css'
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import ReactPlayer from 'react-player';
 
 function VideoForm() {
     const history = useHistory()
@@ -17,19 +18,61 @@ function VideoForm() {
 
     return (
         <div className='VF-Wrapper'>
-            <div className='VF-TopBar'>
-                <p className='VF-Title'>Video Details</p>
-                <span id='VF-X-Symbol' class="material-symbols-outlined"> close </span>
+            <div className='VF-Left-Wrapper'>
+                <div className='VF-TopBar'>
+                    <p className='VF-Title'>Video Details</p>
+                    <span id='VF-X-Symbol' class="material-symbols-outlined"> close </span>
+                </div>
+                <div className='VF-Video-Details-Wrapper'>
+
+                    <div className='VF-VD-Title-Wrapper'>
+                        <p className='VF-CD-Title'>Details</p>
+                    </div>
+
+                    <div className='VF-CD-VT-Wrapper'>
+                        <div className='VF-CD-VT-Input-Wrapper'>
+                            <textarea className='VF-CD-VT-Input' type='text' value={title} placeholder='Title(required)' onChange={(e) => setTitle(e.target.value)} maxLength={100} />
+                        </div>
+                        <p className='VF-CD-VT-Count'>{`${title.length}/100`}</p>
+                    </div>
+
+                    <div className='VF-CD-VD-Wrapper'>
+                        <div className='VF-CD-VD-Input-Wrapper'>
+                            <textarea className='VF-CD-VD-Input' type='text' value={description} placeholder='Description' onChange={(e) => setDescription(e.target.value)} maxLength={1000} />
+                        </div>
+                        <p className='VF-CD-VD-Count'>{`${description.length}/1000`}</p>
+                    </div>
+
+                    <div className='VF-CD-VUrl-Wrapper'>
+                        <div className='VF-CD-VUrl-Input-Wrapper'>
+                            <input className='VF-CD-VUrl-Input' type='text' value={video} placeholder='Video Url' onChange={(e) => setVideo(e.target.value)} maxLength={1000} />
+                        </div>
+                    </div>
+
+                    <div className='VF-CD-VThumbnail-Wrapper'>
+                        <div className='VF-CD-VThumbnail-Input-Wrapper'>
+                            <input className='VF-CD-VThumbnail-Input' type='text' value={thumbnail} placeholder='Thumbnail Url' onChange={(e) => setThumbnail(e.target.value)} maxLength={1000} />
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='VF-Video-Details-Wrapper'>
-                <div className='VF-VD-Title-Wrapper'>
-                    <p className='VF-CD-Title'>Details</p>
+
+            <div className='VF-Right-Wrapper'>
+                <div className='VF-Preview-Wrapper'>
+                    <div className='VF-Player-Wrapper'>
+                        <ReactPlayer
+                            url={video}
+                            controls={true}
+                            width={300}
+                            height={200}
+                        />
+                    </div>
                 </div>
-                <div className='VF-CD-VT-Wrapper'>
-                    <input className='VF-CD-VT-Input' type='text' value={title} placeholder='' onChange={(e) => setTitle(e.target.value)} maxLength={100}/>
-                    <p className='VF-CD-VT-Title'>Title(required)</p>
-                    <p className='VF-CD-VT-Count'>{`${title.length}/100`}</p>
-                </div>
+            </div>
+
+
+            <div className='VF-Footer'>
+                <div className='VF-Footer-Submit' onClick={((e) => '')}>Upload</div>
             </div>
         </div>
     )
