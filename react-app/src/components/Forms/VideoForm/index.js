6dda2f664cc84e35b14f9cc7cefbb2b76a111af6
Yasine Omar Benzekri <105954797+Yasine-ben@users.noreply.github.com>
2023-06-04 16:29:47 -0400

    aws for video upload working

--- react-app/src/components/Forms/VideoForm/index.js
+++ react-app/src/components/Forms/VideoForm/index.js
@@ -4,8 +4,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import uploadGuy from '../../../Images/Upload.svg'
-import { thunkUploadVideo } from '../../../store/video';
+import { thunkAllVideos, thunkUploadVideo } from '../../../store/video';
 import { useModal } from "../../../context/Modal";
+import { useDropzone } from 'react-dropzone';
 
 function VideoForm() {
     const history = useHistory()
@@ -18,79 +19,167 @@ function VideoForm() {
     const [description, setDescription] = useState('')
     const [video, setVideo] = useState('')
     const [thumbnail, setThumbnail] = useState('')
-    const [length, setLength] = useState(1.50)
+    const [thumbnailPreview, setThumbnailPreview] = useState(null);
+    const [videoPreview, setVideoPreview] = useState(null);
     const [errors, setErrors] = useState({})
 
+    const imageFormat = [
+        ".jpg", ".jpeg", ".png", ".gif",
+        ".bmp", ".tiff", ".psd", ".ai",
+        ".eps", ".svg", ".pdf", ".ico",
+        ".raw", ".webp"
+    ];
+    const videoFormat = [
+        ".mp4",
+        ".mov",
+        ".avi",
+        ".mkv",
+        ".wmv",
+        ".flv",
+        ".webm",
+        ".m4v",
+    ];
+    const handleThumbnailFileChange = async (acceptedFiles) => {
+        const file = acceptedFiles[0];
+        if (isImageFile(file)) {
+            setThumbnail(file);
+            delete errors.thumbnail
+        }
+        else return errors.thumbnail = 'not a valid image file'
+
+        // Read the file for preview
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            // Set the file preview as data URL
+            setThumbnailPreview(event.target.result);
+        };
+        reader.readAsDataURL(file);
+    };
+    const handleVideoFileChange = (acceptedFiles) => {
+        const file = acceptedFiles[0];
+
+        if (isVideoFile(file)) {
+            setVideo(file);
+            delete errors.video
+        }
+        else return errors.video = 'not a valid video file'
+
+        // Read the file for preview
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            // Set the file preview as data URL
+            setVideoPreview(event.target.result);
+        };
+        reader.readAsDataURL(file);
+    };
+    function isVideoFile(videoFile) {
+        if (!videoFormat?.some(ext => videoFile?.name.endsWith(ext))) {
+            return false
+        } else {
+            return true
+        }
+    }
+    function isImageFile(imageFile) {
+        if (!imageFormat?.some(ext => imageFile?.name.endsWith(ext))) {
+            return false
+        }
+        else if (imageFile?.name.endsWith('.HEIC')) {
+            errors.thumbnail = ('HEIC')
+        }
+        else {
+            return true
+        }
+    }
+    const isFormValid = () => {
+        return (
+            title.trim().length > 0 &&
+            description !== null &&
+            thumbnail !== null &&
+            video !== null
+        );
+    };
+    const {
+        getRootProps: getThumbnailRootProps,
+        getInputProps: getThumbnailInputProps,
+        isDragActive: isThumbnailDragActive,
+        isDragAccept: isThumbnailDragAccept,
+        isDragReject: isThumbnailDragReject
+    } = useDropzone({
+        accept: 'image/jpeg, image/png',
+        onDrop: handleThumbnailFileChange
+    });
+    const {
+        getRootProps: getVideoRootProps,
+        getInputProps: getVideoInputProps,
+        isDragActive: isVideoDragActive,
+        isDragAccept: isVideoDragAccept,
+        isDragReject: isVideoDragReject
+    } = useDropzone({
+        accept: 'video/mp4, video/mov', // Update the accept property to accept video files
+        onDrop: handleVideoFileChange
+    });
+    
+    //SUBMIT LOGIC///////////////////////////////////////////////////////////////////////////////////////////////
     const handleSubmit = async (e) => {
         e.preventDefault();
         const err = {}
+
         if (title.length < 1) err.title = 'Title must be filled in'
         if (title.length > 100) err.title = 'Title must be less than 101 character in length'
 
         if (description.length < 1) err.description = 'Description must be filled in'
         if (description.length > 1000) err.description = 'Description must be less than 1001 characters in length'
 
-        if (!isValidUrl(video)) err.video = 'Video Url Must be a valid Url'
-        if (!isValidUrl(thumbnail)) err.thumbnail = 'Thumbnail Url must be a valid url'
-
         const uploader = sessionUser.username
         const user_id = sessionUser.id
 
+        if (!imageFormat?.some(ext => thumbnail?.name.endsWith(ext))) {
+            err.thumbnail = 'Please provide a valid image file';
+        } else if (thumbnail?.name.endsWith('.HEIC')) {
+            err.thumbnail = ('HEIC')
+        }
+
+        if (!videoFormat?.some(ext => video?.name.endsWith(ext))) {
+            err.video = 'Please provide a valid audio file';
+        }
+
         if (!uploader || !user_id) err.user = 'You Must Be Logged In'
 
-        if (!length) err.length = 'Video url is invalid'
-
-        if (!isImageUrl(thumbnail)) err.thumbnail = 'Thumbnail url must lead to an image'
-        
         if (!Object.values(err).length) {
             setErrors(err)
-            const data = await dispatch(thunkUploadVideo(title, description, video, length, thumbnail, uploader, user_id));
+            const formData = new FormData();
 
-            if (data) {
-                // console.log('SERVER ERRORS')
-                // console.log(data)
-            }
-            else {
-                // console.log("SUBMITTED")
-                closeModal()
-                history.push('/')
-                
-            }
+            formData.append('title', title);
+            formData.append('description', description);
+            formData.append('video', video);
+            formData.append('thumbnail', thumbnail);
+            formData.append('uploader', sessionUser.username);
+            formData.append('user_id', sessionUser.id);
 
-            return
+            const res = await fetch('/api/videos/createVideo', {
+                method: "POST",
+                body: formData
+            });
+            if (res.ok) {
+                closeModal();
+                dispatch(thunkAllVideos);
+                return history.push('/');
+            } else {
+                // setLoading('hidden')
+                err.submit = 'Error with submission please try again'
+                setErrors(err)
+            }
         } else {
-            setErrors(err)
-            // console.log(err)
-            // console.log('FRONT END ERROR FRONT END ERROR')
-            return
-        }
-
-    }
-
-    function handleReady() {
-        let len = (playerRef.current.getDuration());
-        len = (len / 60).toFixed(2)
-        setLength(Number(len))
-    }
-
-    function isValidUrl(string) {
-        try {
-            new URL(string);
-            return true;
-        } catch (_) {
-            return false;
+            return setErrors(err)
         }
     }
+    //SUBMIT LOGIC///////////////////////////////////////////////////////////////////////////////////////////////
 
-    function isImageUrl(url) {
-        const imageExtensions = /\.(jpg|jpeg|png|gif)$/i;
-        return imageExtensions.test(url);
-    }
 
     return (
         sessionUser && (<div className='VF-Wrapper'>
             <div className='VF-Left-Wrapper'>
-                <div className='VF-TopBar'>
+                {/* <div className='VF-TopBar'>
                     <p className='VF-Title'>Upload Video</p>
                     <span onClick={(e) => {closeModal()}} id='VF-X-Symbol' className="material-symbols-outlined"> close </span>
                 </div>
@@ -126,39 +215,146 @@ function VideoForm() {
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> */}
 
-            <div className='VF-Right-Wrapper'>
-                <div className='VF-Preview-Wrapper'>
-                    <div className='VF-Player-Wrapper'>
-                        <ReactPlayer
-                            ref={playerRef}
-                            url={video}
-                            controls={true}
-                            width={300}
-                            height={200}
-                            onReady={handleReady}
+                <form className='' onSubmit={handleSubmit} method="POST" encType="multipart/form-data">
+                    {errors.titleLength ? <div className="">* {errors.titleLength}</div> : null}
+                    <div className=''>
+                        Video Title
+                        <input
+                            type="text"
+                            name="videoTitle"
+                            onChange={(e) => setTitle(e.target.value)}
+                            value={title}
+                            placeholder="Title"
+                            className=""
                         />
+                        {errors.title && <p className="">{errors.title}</p>}
                     </div>
+
+                    <div className=''>
+                        Video Description
+                        <input
+                            type="text"
+                            name="videoDescription"
+                            onChange={(e) => setDescription(e.target.value)}
+                            value={description}
+                            placeholder="Description"
+                            className=""
+                        />
+                        {errors.description && <p className="">{errors.description}</p>}
+                    </div>
+
+                    <div className="">
+                        <div {...getThumbnailRootProps({ className: `dropzone ${isThumbnailDragActive ? "active" : ""} ${isThumbnailDragAccept ? "accept" : ""} ${isThumbnailDragReject ? "reject" : ""}` })}>
+                            <input {...getThumbnailInputProps()} />
+                            {!isThumbnailDragActive && !thumbnailPreview && (<i class="fa-solid fa-image" id='CI-Cloud'></i>)}
+                            {!isThumbnailDragActive && !thumbnailPreview && !errors.thumbnail && (<p>Tap or drop an image here</p>)}
+                            {errors.thumbnail && <p className="" style={{ color: 'red' }}>Invalid file format for thumbnail image. Please upload a JPEG or PNG file.</p>}
+                            {thumbnailPreview && <div className=""><img src={thumbnailPreview} alt="Thumbnail Preview" className="" /></div>}
+                        </div>
+                    </div>
+
+                    <div className=""></div>
+                    <div {...getVideoRootProps({ className: `dropzone ${isVideoDragActive ? "active" : ""} ${isVideoDragAccept ? "accept" : ""} ${isVideoDragReject ? "reject" : ""}` })}>
+                        <input {...getVideoInputProps()} />
+                        {!isVideoDragActive && !videoPreview && (<i class="fa-solid fa-music" id='CI-Cloud'></i>)}
+                        {!isVideoDragActive && !videoPreview && !errors.video && (<p>Tap or drop your video here</p>)}
+                        {errors.video && <p className="" style={{ color: 'red' }}>Invalid file format for Video file. Please upload an mp4 file.</p>}
+                        {videoPreview && <div className=""><video src={videoPreview} controls className="" /></div>}
+                    </div>
+
+                    <button className='' type="submit" disabled={!isFormValid()}>Submit</button>
+                    <div className="">
+                        {/* <img src={loadingGif} alt='loading-gif' className={loading}></img> */}
+                    </div>
+                </form>
+
+                {/* <div className='VF-Errors-Wrapper'>
+                    {Boolean(Object.values(errors).length) && (<p>Errors</p>)}
+                    {Object.values(errors).map((error, idx) => (
+                        <p className='VF-Errors' key={idx}>{`* ${error}`}</p>
+                    ))}
                 </div>
-            </div>
 
-            <div className='VF-Errors-Wrapper'>
-                {Boolean(Object.values(errors).length) && (<p>Errors</p>)}
-                {Object.values(errors).map((error, idx) => (
-                    <p className='VF-Errors' key={idx}>{`* ${error}`}</p>
-                ))}
-            </div>
+                <div className='VF-Img-Wrapper'>
+                    <img className='VF-Img' src={uploadGuy} alt='upload guy' />
+                </div>
 
-            <div className='VF-Img-Wrapper'>
-                <img className='VF-Img' src={uploadGuy} alt='upload guy' />
+                <div className='VF-Footer'>
+                    <div className='VF-Footer-Submit' onClick={((e) => handleSubmit(e))}>Upload</div>
+                </div> */}
             </div>
-
-            <div className='VF-Footer'>
-                <div className='VF-Footer-Submit' onClick={((e) => handleSubmit(e))}>Upload</div>
-            </div>
-        </div>)
+        </div>
+        )
     )
 }
 
 export default VideoForm;
+
+// const handleSubmit = async (e) => {
+//     e.preventDefault();
+    // const err = {}
+    // if (title.length < 1) err.title = 'Title must be filled in'
+    // if (title.length > 100) err.title = 'Title must be less than 101 character in length'
+
+    // if (description.length < 1) err.description = 'Description must be filled in'
+    // if (description.length > 1000) err.description = 'Description must be less than 1001 characters in length'
+
+    // if (!isValidUrl(video)) err.video = 'Video Url Must be a valid Url'
+    // if (!isValidUrl(thumbnail)) err.thumbnail = 'Thumbnail Url must be a valid url'
+
+    // const uploader = sessionUser.username
+    // const user_id = sessionUser.id
+
+    // if (!uploader || !user_id) err.user = 'You Must Be Logged In'
+
+    // if (!length) err.length = 'Video url is invalid'
+
+    // if (!isImageUrl(thumbnail)) err.thumbnail = 'Thumbnail url must lead to an image'
+
+    // if (!Object.values(err).length) {
+    //     setErrors(err)
+    //     const data = await dispatch(thunkUploadVideo(title, description, video, length, thumbnail, uploader, user_id));
+
+    //     if (data) {
+    //         // console.log('SERVER ERRORS')
+    //         // console.log(data)
+    //     }
+    //     else {
+    //         // console.log("SUBMITTED")
+    //         closeModal()
+    //         history.push('/')
+
+    //     }
+
+    //     return
+    // } else {
+    //     setErrors(err)
+    //     // console.log(err)
+    //     // console.log('FRONT END ERROR FRONT END ERROR')
+    //     return
+    // }
+
+
+// }
+
+// function handleReady() {
+//     let len = (playerRef.current.getDuration());
+//     len = (len / 60).toFixed(2)
+//     setLength(Number(len))
+// }
+
+// function isValidUrl(string) {
+//     try {
+//         new URL(string);
+//         return true;
+//     } catch (_) {
+//         return false;
+//     }
+// }
+
+// function isImageUrl(url) {
+//     const imageExtensions = /\.(jpg|jpeg|png|gif)$/i;
+//     return imageExtensions.test(url);
+// }
