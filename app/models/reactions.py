0f328ac7245ea03e889b81e55e0946a180f997b1
Yasine Omar Benzekri <105954797+Yasine-ben@users.noreply.github.com>
2023-06-13 22:54:18 -0400

    change

--- app/models/reactions.py
+++ app/models/reactions.py
@@ -1,10 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-from sqlalchemy import Enum
-
-
-reaction_type_enum = Enum('reaction_type_enum', 'like', 'dislike')
+from sqlalchemy import CheckConstraint
 
 class Reaction(db.Model):
     __tablename__ = 'reactions'
@@ -15,15 +12,20 @@ class Reaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     video_id = db.Column(db.Integer, db.ForeignKey('videos.id'))
-    reaction_type = db.Column(reaction_type_enum, nullable=False)
+    reaction_type = db.Column(db.String(10), nullable=False)
+    
+    # Add a CheckConstraint to enforce the allowed values
+    __table_args__ = (
+        CheckConstraint(reaction_type.in_(['like', 'dislike']), name='reaction_type_check'),
+    )
 
     user = db.relationship('User', back_populates='reactions')
     video = db.relationship('Video', back_populates='reactions')
 
     def to_dict(self):
-            return {
-                'id': self.id,
-                'user_id': self.user_id,
-                'video_id': self.video_id,
-                'reaction_type': self.reaction_type,
-            }
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'video_id': self.video_id,
+            'reaction_type': self.reaction_type,
+        }
